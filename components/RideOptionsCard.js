5cbc7336ef395255b0136885c4ad4e370c84d34d
Huseyin Kara <67840358+benhuseyin@users.noreply.github.com>
2023-03-25 01:37:22 +0300

    uber-clone
    This project have done as an exercise to learn more react-native.
    
    I used https://www.youtube.com/watch?v=AkEnidfZnCU this video as a source.

---
+++ components/RideOptionsCard.js
@@ -0,0 +1,97 @@
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Image, FlatList } from 'react-native'
+import React, { useState } from 'react'
+import tw from 'tailwind-react-native-classnames'
+import {Icon} from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
+import { useSelector } from 'react-redux'
+import { selectTravelTimeInformation } from '../slices/navSlice'
+
+
+
+const data =[
+    {
+        id: "Uber-X-123",
+        title: "UberX",
+        multiplier: 1,
+        image: "https://links.papareact.com/3pn"
+    },
+    {
+        id: "Uber-XL-456",
+        title: "UberXL",
+        multiplier: 1.2,
+        image: "https://links.papareact.com/5w8"
+    },
+    {
+        id: "Uber-LUX-789",
+        title: "Uber LUX",
+        multiplier: 1.75,
+        image: "https://links.papareact.com/7pf"
+    }
+]
+
+const SURGE_CHARGE_RATE = 1.5;
+
+
+const RideOptionsCard = () => {
+
+    const navigation = useNavigation();
+    const [selected, setSelected] = useState(null);
+    const travelTimeInformation = useSelector(selectTravelTimeInformation);
+
+  return (
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+        <View style={tw`flex-row`}>
+            <TouchableOpacity onPress={()=>navigation.navigate("NavigateCard")} style={tw`top-3 left-5 p-3 rounded-full`}>
+                <Icon name="chevron-left" type="font-awesome"/>
+            </TouchableOpacity>
+            <View style={{width:330}}>
+                <Text style={tw`text-center py-5 text-xl `}>Select a Ride - {travelTimeInformation?.distance?.text}</Text>
+            </View>
+            
+        </View>
+        <View>
+            <TouchableOpacity disabled={!selected} style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-200"}`}>
+                <Text style={tw`text-center text-white text-xl`}>Choose {selected?.title}</Text>
+            </TouchableOpacity>
+        </View>
+        <FlatList
+            data={data}
+            keyExtractor={(item)=>item.id}
+            renderItem={({item:{id,title,multiplier,image}, item})=>(
+                <TouchableOpacity style={tw`flex-row justify-between items-center px-10 ${id===selected?.id && "bg-gray-200"}`} onPress={()=>{setSelected(item)}}>
+                    
+                    <Image 
+                        style={{
+                            width:100,
+                            height:100,
+                            resizeMode:"contain",
+                            marginLeft:-15
+                        }}
+                        source= {{uri:image}}
+                    />
+                    <View style={tw`ml-1 mr-3`}>
+                        <Text style={tw`text-xl font-semibold`}>{title}</Text>
+                        <Text>{travelTimeInformation?.duration.text} - Travel Time</Text>
+                    </View>
+                    <Text style={tw`text-xl`}>
+
+                        {new Intl.NumberFormat('en-gb', {
+                            style: 'currency',
+                            currency: 'GBP'
+                        }).format(
+                            (travelTimeInformation?.duration.value * SURGE_CHARGE_RATE * multiplier / 100)
+                        )}
+
+                    </Text>
+                </TouchableOpacity>
+            )}
+        />
+
+      
+    </SafeAreaView>
+  )
+}
+
+export default RideOptionsCard
+
+const styles = StyleSheet.create({})
